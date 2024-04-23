e9883899af4b1ba94969dbae3146463d6a6db233
Lynxist <jimbobish@gmail.com>
2024-04-23 13:25:56 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,22 @@
+fx_version 'adamant'
+games {'gta5'}
+
+author 'Lynxist'
+description 'Loot Zones'
+version '1.0'
+
+ui_page 'html/index.html'
+
+client_scripts {
+	'@PolyZone/client.lua',
+  '@PolyZone/BoxZone.lua',
+  '@PolyZone/EntityZone.lua',
+  '@PolyZone/CircleZone.lua',
+  '@PolyZone/ComboZone.lua',
+	"client/*.lua",
+}
+
+server_scripts {
+  "server/*.lua",
+}
+
